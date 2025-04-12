003da40ac3f67cd79a201d4c307c9dd2ec87fc38
Mike Horrocks <michael.horrocks@hotmail.co.uk>
2025-04-12 15:00:32 +0100

    Adding local resources

--- cmsproject/urls.py
+++ cmsproject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
@@ -22,13 +23,14 @@ from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = i18n_patterns(
-    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-    path('admin/', admin.site.urls),
-    path('filer/', include('filer.urls')),
-    path('', include('cms.urls')),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
+    path("admin/", admin.site.urls),
+    path("filer/", include("filer.urls")),
+    path("", include("cms.urls")),
 )
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
